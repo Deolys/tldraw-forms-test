60125dbb3a324115853f23e4fc55724456e1ffb4
Deolys <ed03aleks@mail.ru>
2024-10-06 19:01:26 +0300

    feat: add base shape creation tool

---
+++ src/utils/custom-tools/create-base-shape.tsx
@@ -0,0 +1,42 @@
+import {
+  DEFAULT_SUPPORTED_MEDIA_TYPE_LIST,
+  FileHelpers,
+  MediaHelpers,
+  StateNode,
+} from "tldraw";
+
+const OFFSET = 100;
+export class BaseShapeTool extends StateNode {
+  static override id = "base-shape";
+
+  override onEnter() {
+    this.editor.setCursor({ type: "cross", rotation: 0 });
+  }
+
+  override onPointerDown() {
+    const { currentPagePoint } = this.editor.inputs;
+
+    const input = window.document.createElement("input");
+    input.type = "file";
+    input.accept = DEFAULT_SUPPORTED_MEDIA_TYPE_LIST;
+    input.addEventListener("change", async (e) => {
+      const fileList = (e.target as HTMLInputElement).files;
+      if (!fileList || fileList.length === 0) return;
+      const file = fileList[0];
+
+      try {
+        const imageUrl = await FileHelpers.blobToDataUrl(file);
+        const { w, h } = await MediaHelpers.getImageSize(file);
+        this.editor.createShape({
+          type: "base-shape",
+          x: currentPagePoint.x - OFFSET,
+          y: currentPagePoint.y - OFFSET,
+          props: { text: "Text", image: imageUrl, w, h },
+        });
+      } catch (e) {
+        console.error(e);
+      }
+    });
+    input.click();
+  }
+}
